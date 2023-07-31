193deb27b3f1e090fd3544ba4b00ae1f88b14c5a
jatkrishan <krishanchoudhary13@gmail.com>
2023-07-31 19:14:00 +0530

    fix db name

--- config/configUrl.js
+++ config/configUrl.js
@@ -1,4 +1,4 @@
 module.exports = {
-    DB_NAME: "CRM_APP" ,
-    DB_URL: "mongodb://127.0.0.1/crm_db"
+    DB_NAME: "crm_app" ,
+    DB_URL: "mongodb://0.0.0.0/"
 }
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
 const mongoose = require("mongoose")
-mongoose.connect(configUrl.DB_URL)
+mongoose.connect(configUrl.DB_URL+configUrl.DB_NAME)
 const db = mongoose.connection
 
 
