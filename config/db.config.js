4f03c232bd7271a7eca58a953e935414f5645a5b
jatkrishan <krishanchoudhary13@gmail.com>
2023-08-02 17:03:17 +0530

    Update Config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
    DB_NAME: "crm_db",
-//   DB_URL: process.env.MONGODB_URI  || "mongod://127.0.0.1/crm_db"
+  DB_URL: process.env.MONGODB_URI  || "mongod://127.0.0.1/crm_db"
    // DB_URL: "mongod://localhost/crm_db"
-     DB_URL: "mongodb+srv://krishanchoudhary43:8432033943@crm-db.tfx8qqw.mongodb.net/"
+   //   DB_URL: "mongodb+srv://krishanchoudhary43:8432033943@crm-db.tfx8qqw.mongodb.net/"
 } 
